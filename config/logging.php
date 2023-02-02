b256cdd520336a7bd9f36ead67bffe52b424cf71
Александр Вакулов <al.vakulov@gmail.com>
2023-02-02 12:42:12 +0700

    stage 1

--- config/logging.php
+++ config/logging.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Logging\Telegram\TelegramLoggerFactory;
 use Monolog\Handler\NullHandler;
 use Monolog\Handler\StreamHandler;
 use Monolog\Handler\SyslogUdpHandler;
@@ -117,6 +118,14 @@ return [
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
         ],
+
+        'telegram' => [
+            'driver' => 'custom',
+            'via' => TelegramLoggerFactory::class,
+            'level' => env('LOG_LEVEL', 'debug'),
+            'chat_id' => -620779609,
+            'token' => '5995925073:AAF-uyOykxtWX2XRWzYiXFWaqCa8cRx0R4k'
+        ]
     ],
 
 ];
